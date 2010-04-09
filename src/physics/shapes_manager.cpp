8e5e4be8d0293507244d14c2d9920888c090721c
Jon Ander Peñalba <jonan88@gmail.com>
2010-04-09 19:11:14 +0200

    Start implementing a manager for the collision shapes. (Only boxes are supported)

---
+++ src/physics/shapes_manager.cpp
@@ -0,0 +1,64 @@
+/*
+This file is part of Freedom to Smash.
+Copyright (C) 2009 Jon Ander Peñalba <jonan88@gmail.com>
+
+Freedom to Smash is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include "shapes_manager.hpp"
+
+#include <iostream>
+
+#include <boost/foreach.hpp>
+
+#include <btBulletCollisionCommon.h>
+
+namespace physics {
+
+// Singleton pattern constructor
+ShapesManager* ShapesManager::getInstance(void) {
+    static ShapesManager instance;
+    return &instance;
+}
+
+// Destructor
+ShapesManager::~ShapesManager(void) {
+    BOOST_FOREACH(btBoxShape *shape, box_shapes)
+        delete shape;
+}
+
+// Function to get a box shape.
+btBoxShape* ShapesManager::getBoxShape(const float width, const float height, const float depth) {
+    btBoxShape* temp = findBoxShape(width, height, depth);
+    if (!temp) {
+        temp = new btBoxShape(btVector3(width/2, height/2, depth/2));
+        box_shapes.push_back(temp);
+    }
+    return temp;
+}
+
+// Functions to find a given box shape.
+btBoxShape* ShapesManager::findBoxShape(const float width, const float height, const float depth) {
+    btBoxShape* temp = NULL;
+    btVector3 half_size;
+    BOOST_FOREACH(btBoxShape *shape, box_shapes) {
+        if (!temp) {
+            half_size = shape->getHalfExtentsWithMargin();
+            if (half_size.x() == width/2 && half_size.y() == height/2 && half_size.z() == depth/2)
+                temp = shape;
+        }
+    }
+    return temp;
+}
+
+} // namespace physics
---
+++ src/physics/shapes_manager.hpp
@@ -0,0 +1,60 @@
+/*
+This file is part of Freedom to Smash.
+Copyright (C) 2009 Jon Ander Peñalba <jonan88@gmail.com>
+
+Freedom to Smash is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+/// @file
+/// The physics::ShapesManager class.
+/// @author Jonan
+
+#ifndef PHYSICS_SHAPES_MANAGER_HPP
+#define PHYSICS_SHAPES_MANAGER_HPP
+
+#include <list>
+
+#include "util.hpp"
+
+// Bullet classes
+class btBoxShape;
+
+namespace physics {
+
+/// Stores all physic shapes to make sure there isn't any duplicated.
+class ShapesManager {
+public:
+    static ShapesManager* getInstance(void); // Singleton pattern constructor
+    ~ShapesManager(void); // Destructor
+
+    // @{
+    /// Functions to get the different physic shapes.
+    btBoxShape* getBoxShape(const float width, const float height, const float depth);
+    // @}
+
+private:
+    ShapesManager(void) {} // Constructor
+
+    // @{
+    // Functions to find a given shape.
+    btBoxShape* findBoxShape(const float width, const float height, const float depth);
+    // @}
+
+    std::list<btBoxShape*> box_shapes;
+
+    DISALLOW_COPY_AND_ASSIGN(ShapesManager);
+};
+
+} // namespace physics
+
+#endif // PHYSICS_SHAPES_MANAGER_HPP
