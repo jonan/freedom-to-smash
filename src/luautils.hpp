c89afbdb7f2dfae086ddee4ff0a5bb1b52c2a868
Luis <zstars@gmail.com>
2010-04-14 00:09:45 +0200

    Removed Andrew Lim's functions from luautils to avoid license issues. LuaEvaluator can be used instead.

--- src/luautils.hpp
+++ src/luautils.hpp
@@ -1,12 +1,7 @@
-// Archivo: luautils.h
-// 
-// Creditos: Este archivo ha sido creado por Andrew Lim
-// http://windrealm.com/tutorials/reading-a-lua-configuration-file-from-c.php#downloads
-//
 
 
-#ifndef WINDREALM_LUA_UTILS_H
-#define WINDREALM_LUA_UTILS_H
+#ifndef _LUA_UTILS_H
+#define _LUA_UTILS_H
 
 extern "C"
 {
@@ -16,33 +11,6 @@ extern "C"
 }
 
 #include <Ogre.h>
-
-
-
-/**
-Evaluates a Lua expression and returns the string result.
-If an error occurs or the result is not string, def is returned.
-*/
-const char* lua_stringexpr( lua_State* L, const char* expr, const char* def );
-
-/**
-Evaluates a Lua expression that results in a number.
-The number is stored in out.
-Returns 0 if there's an error, 1 otherwise.
-*/
-int lua_numberexpr( lua_State* L, const char* expr, double* out );
-
-/**
-Evaluates a Lua expression that results in a number and cast to an int.
-Returns 0 if there's an error, 1 otherwise.
-*/
-int lua_intexpr( lua_State* L, const char* expr, int* out );
-
-/**
-Evaluates a Lua expression that results in a number.
-Returns 0 if there's an error, 1 otherwise.
-*/
-int lua_boolexpr( lua_State* L, const char* expr );
 
 
 Ogre::Vector3 lua_vectorexpr( lua_State * L, const char * expr, 
