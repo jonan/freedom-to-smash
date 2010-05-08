f19a84cdadbcb06fb9a029643cf1cba1b55e31b0
Luis <zstars@gmail.com>
2010-05-08 19:10:47 +0200

    Added script manager files. Not yet implemented.

---
+++ src/script_manager.cpp
@@ -0,0 +1,19 @@
+/*
+This file is part of Freedom to Smash.
+Copyright (C) 2009-2010 FTS Team
+
+Freedom to Smash is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+
+#include "script_manager.hpp"
