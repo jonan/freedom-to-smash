b30732838bffdb6caebd176e756ebbb17e569aee
Luis <zstars@gmail.com>
2010-05-09 21:30:49 +0200

    Added PrintMethods debugging function to config.lua script.

--- scripts/config.lua
+++ scripts/config.lua
@@ -10,3 +10,24 @@ Config =
 
 
 print "[DLua] Config"
+
+
+
+
+function PrintMethods(obj)
+	print("****************************")
+	print("Print: ", obj)
+	print("SwigType: ", swig_type(obj))
+	print("METHODS:")
+	local meta = getmetatable(obj)
+	print(meta)
+	if meta == nil then
+		print("No methods.")
+		print("****************************")
+		return
+	end
+	local funtable = meta['.fn']
+	--table.foreach(funtable, print)
+	for i, v in pairs(funtable) do print(i, v) end
+	print("****************************")
+end
