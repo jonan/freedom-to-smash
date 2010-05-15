18303d4270d44d13821b1fa4e077c4adb7d7ab43
Jon Ander Peñalba <jonan88@gmail.com>
2010-05-15 17:40:42 +0200

    The project now builds with Ogre 1.6.4

--- scripts/config.lua
+++ scripts/config.lua
@@ -6,10 +6,10 @@ Config =
     MajorVersion = 0,
     MinorVersion = 2,
     Num = 3.14,
-	Player1 = "sinbad",
-	Player2 = "sinbad",
-	Hydrax = true,
-	SkyX = true
+    Player1 = "sinbad",
+    Player2 = "sinbad",
+    Hydrax = false,
+    SkyX = false
 }
 
 
@@ -19,19 +19,19 @@ print "[DLua] Config"
 
 
 function PrintMethods(obj)
-	print("****************************")
-	print("Print: ", obj)
-	print("SwigType: ", swig_type(obj))
-	print("METHODS:")
-	local meta = getmetatable(obj)
-	print(meta)
-	if meta == nil then
-		print("No methods.")
-		print("****************************")
-		return
-	end
-	local funtable = meta['.fn']
-	--table.foreach(funtable, print)
-	for i, v in pairs(funtable) do print(i, v) end
-	print("****************************")
+    print("****************************")
+    print("Print: ", obj)
+    print("SwigType: ", swig_type(obj))
+    print("METHODS:")
+    local meta = getmetatable(obj)
+    print(meta)
+    if meta == nil then
+        print("No methods.")
+        print("****************************")
+        return
+    end
+    local funtable = meta['.fn']
+    --table.foreach(funtable, print)
+    for i, v in pairs(funtable) do print(i, v) end
+    print("****************************")
 end
