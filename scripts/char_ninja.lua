8391df19ffb3ee64da7c6dd3b73bf4797cb7a455
Jon Ander Peñalba <jonan88@gmail.com>
2010-05-18 18:25:09 +0200

    Character scripts updated (mass added)

--- scripts/char_ninja.lua
+++ scripts/char_ninja.lua
@@ -1,29 +1,29 @@
 
 Character =
 {
-	Name = "ninja",
-	Position = ogre.Vector3(0,5,0),
-	Size = ogre.Vector3(3.0, 10.0, 2.0),
-	Yaw = -90,
-	Scale = 0.05,
-	
-	OnCreate = 
-		function( ch )
-			--ch:attachEntityToBone("Sword", "Handle.L")
-			--ch:attachEntityToBone("Sword", "Handle.R")
-			
-			-- Other anims: Backflip Climb Crouch Jump JumpNoHeight Kick SideKick Spin Stealth
-			-- Walk Death1 Death2 HighJump
-			
-			ch:createAnimation(fts.Character_ATTACK, "Attack3")
-			ch:createAnimation(fts.Character_DEFEND, "Block");
-			ch:createAnimation(fts.Character_FALL, "JumpNoHeight", true);
-			ch:createAnimation(fts.Character_IDLE, "Idle1", true);
-			--ch:createAnimation(fts.Character_IDLE, "Idle3", true);
-			ch:createAnimation(fts.Character_JUMP, "JumpNoHeight");
-			ch:createAnimation(fts.Character_LAND, "Crouch");
-			ch:createAnimation(fts.Character_MOVE, "Walk", true);
-			--ch:createAnimation(fts.Character_MOVE, "RunBase", true);
-		end,
+    Name = "ninja",
+    Position = ogre.Vector3(0,5,0),
+    Mass = 20,
+    Size = ogre.Vector3(3.0, 10.0, 2.0),
+    Yaw = -90,
+    Scale = 0.05,
+
+    OnCreate =
+        function( ch )
+            --ch:attachEntityToBone("Sword", "Handle.L")
+            --ch:attachEntityToBone("Sword", "Handle.R")
+
+            -- Other anims: Backflip Climb Crouch Jump JumpNoHeight Kick SideKick Spin Stealth
+            -- Walk Death1 Death2 HighJump
+
+            ch:createAnimation(fts.Character_ATTACK, "Attack3")
+            ch:createAnimation(fts.Character_DEFEND, "Block");
+            ch:createAnimation(fts.Character_FALL, "JumpNoHeight", true);
+            ch:createAnimation(fts.Character_IDLE, "Idle1", true);
+            --ch:createAnimation(fts.Character_IDLE, "Idle3", true);
+            ch:createAnimation(fts.Character_JUMP, "JumpNoHeight");
+            ch:createAnimation(fts.Character_LAND, "Crouch");
+            ch:createAnimation(fts.Character_MOVE, "Walk", true);
+            --ch:createAnimation(fts.Character_MOVE, "RunBase", true);
+        end,
 }
-	
--- scripts/char_sinbad.lua
+++ scripts/char_sinbad.lua
@@ -1,25 +1,25 @@
 
 Character =
 {
-	Name = "sinbad",
-	Position = ogre.Vector3(0,5,0),
-	Size = ogre.Vector3(3.0, 10.0, 2.0),
-	Yaw = 90,
-	
-	OnCreate = 
-		function( ch )
-			ch:attachEntityToBone("Sword", "Handle.L")
-			ch:attachEntityToBone("Sword", "Handle.R")
-			
-			ch:createAnimation(fts.Character_ATTACK, "SliceVertical")
-			ch:createAnimation(fts.Character_DEFEND, "DrawSwords");
-			ch:createAnimation(fts.Character_FALL, "JumpLoop", true);
-			ch:createAnimation(fts.Character_IDLE, "IdleTop", true);
-			ch:createAnimation(fts.Character_IDLE, "IdleBase", true);
-			ch:createAnimation(fts.Character_JUMP, "JumpStart");
-			ch:createAnimation(fts.Character_LAND, "JumpEnd");
-			ch:createAnimation(fts.Character_MOVE, "RunTop", true);
-			ch:createAnimation(fts.Character_MOVE, "RunBase", true);
-		end,
+    Name = "sinbad",
+    Position = ogre.Vector3(0,5,0),
+    Mass = 20,
+    Size = ogre.Vector3(3.0, 10.0, 2.0),
+    Yaw = 90,
+
+    OnCreate =
+        function( ch )
+            ch:attachEntityToBone("Sword", "Handle.L")
+            ch:attachEntityToBone("Sword", "Handle.R")
+
+            ch:createAnimation(fts.Character_ATTACK, "SliceVertical")
+            ch:createAnimation(fts.Character_DEFEND, "DrawSwords");
+            ch:createAnimation(fts.Character_FALL, "JumpLoop", true);
+            ch:createAnimation(fts.Character_IDLE, "IdleTop", true);
+            ch:createAnimation(fts.Character_IDLE, "IdleBase", true);
+            ch:createAnimation(fts.Character_JUMP, "JumpStart");
+            ch:createAnimation(fts.Character_LAND, "JumpEnd");
+            ch:createAnimation(fts.Character_MOVE, "RunTop", true);
+            ch:createAnimation(fts.Character_MOVE, "RunBase", true);
+        end,
 }
-	
