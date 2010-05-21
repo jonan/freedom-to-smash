7285255bf88ee71c07c81f70b06dca4eda374f77
Jon Ander Peñalba <jonan88@gmail.com>
2010-05-21 17:27:17 +0200

    physics::ObjectSynchronizer class added

---
+++ src/physics/object_synchronizer.cpp
@@ -0,0 +1,37 @@
+/*
+This file is part of Freedom to Smash.
+Copyright (C) 2009 Jon Ander Peñalba <jonan88@gmail.com>
+
+Freedom to Smash is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+#include <physics/object_synchronizer.hpp>
+
+// FtS
+#include <physics/converter_functions.hpp>
+
+namespace physics {
+
+// Get the object's position.
+void ObjectSynchronizer::getWorldTransform(btTransform &transform) const
+{
+    transform.setOrigin(vector3(getPosition()));
+}
+
+// Set the object's position.
+void ObjectSynchronizer::setWorldTransform(const btTransform &transform)
+{
+    setPosition(vector3(transform.getOrigin()));
+}
+
+} // namespace physics
---
+++ src/physics/object_synchronizer.hpp
@@ -0,0 +1,57 @@
+/*
+This file is part of Freedom to Smash.
+Copyright (C) 2009 Jon Ander Peñalba <jonan88@gmail.com>
+
+Freedom to Smash is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+/// @file
+/// The physics::ObjectSynchronizer class.
+/// @author Jonan
+
+#ifndef PHYSICS_OBJECT_SYNCHRONIZER_HPP
+#define PHYSICS_OBJECT_SYNCHRONIZER_HPP
+
+// Bullet
+#include <LinearMath/btMotionState.h>
+
+// FtS
+#include <util.hpp>
+
+namespace physics {
+
+/// Class to synchronize a physic object.
+class ObjectSynchronizer : public btMotionState {
+public:
+    ObjectSynchronizer(void) {} // Constructor
+    virtual ~ObjectSynchronizer(void) {} // Destructor
+
+    // @{
+    /// The physics system will call this methods when convenient.
+    virtual const Ogre::Vector3& getPosition(void) const = 0;
+    virtual void setPosition(const Ogre::Vector3 &pos) = 0;
+    // @}
+
+private:
+    // @{
+    // Bullet's methods to manage the object's position.
+    virtual void getWorldTransform(btTransform &transform) const;
+    virtual void setWorldTransform(const btTransform &transform);
+    // @}
+
+    DISALLOW_COPY_AND_ASSIGN(ObjectSynchronizer);
+};
+
+} // namespace physics
+
+#endif // PHYSICS_OBJECT_SYNCHRONIZER_HPP
